03c664a0665c11cfd28255177e355bae33c3653a
telegin-code <teleghin.a@gmail.com>
2025-11-17 21:09:32 +0000

    new file:   scoring.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("12 Motivations app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
